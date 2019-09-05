17659df7684fc9565731e33557fadc4ec0c228b2
John Vitales <JohnieeJuiceboxx@gmail.com>
2019-09-05 16:56:58 -0400

    change

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -2,7 +2,19 @@
 
 // Complete this algo
 const minJumps = arr => {
-
+  let start = 0
+  let nummove = arr[start]
+  let max = arr[start+1]
+  let end = arr.length - 1
+  while(){
+    for(i = start; i<start+nummove-1 ; i++ ){
+      if(max < arr[i+2]){
+        max = arr[i+2]
+        nummove = arr[i+2]
+        start = i+2
+      }
+    }
+  }
 };
 
-module.exports = minJumps
+module.exports = minJumps;
