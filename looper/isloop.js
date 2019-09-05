f431ebd2ae080e183e4a232d17dc1d2e31677482
John Vitales <JohnieeJuiceboxx@gmail.com>
2019-09-05 17:13:38 -0400

    pushin

--- looper/isloop.js
+++ looper/isloop.js
@@ -1,10 +1,21 @@
 'use strict';
 
 //Complete this algo
-const isLoop = (linkedlist) => {
+const isLoop = linkedlist => {
+  const container = [];
+  // console.log(linkedlist);
+  let temp = linkedlist.head;
 
+  while (!container.includes(temp.value)) {
+    if (temp.next === null) {
+      return false;
+    }
+    container.push(temp.value);
+    // console.log(container);
+    temp = temp.next;
+  }
+  return true;
 };
-
 
 /*
 EXTRA CREDIT:
@@ -14,4 +25,4 @@ This function should return the Node value the loop begins at
 Remember to write some test specs too!
 
 */
-module.exports = isLoop
+module.exports = isLoop;
