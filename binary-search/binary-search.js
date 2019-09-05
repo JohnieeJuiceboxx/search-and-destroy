b889c28f0a75b846300b24f2ebab536a9ae55eb3
John Vitales <JohnieeJuiceboxx@gmail.com>
2019-09-05 15:43:33 -0400

    switch roles

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,7 +2,42 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+  // if (array.length > 1) {
+  //   let mid = Math.ceil(array.length / 2);
+  //   console.log(mid);
+  //   let left = array.slice(0, mid);
+  //   let right = array.slice(mid);
+  //   console.log(left);
+  //   console.log(right);
+  //   if (target <= left[left.length - 1]) {
+  //     return binarySearch(left, target);
+  //   } else {
+  //     return binarySearch(right, target);
+  //   }
+  // } else {
+  //   console.log(array[0]);
+  //   if (target === array[0]) {
+  //     console.log('trueeeee');
+  //     return true;
+  //   } else {
+  //     console.log('falseeee');
+  //     return false;
+  //   }
+  // }
+
+  //ExtraCredit
+  if (array.length > 1) {
+    let mid = Math.ceil(array.length / 2);
+    if (array[mid - 1] <= target) {
+      //left
+      mid = Math.ceil(mid / 2);
+      if (array[mid - 1] <= target) {
+      }
+    } else {
+      //right
+      mid = mid + Math.ceil(mid / 2);
+    }
+  }
 };
 
 /*
@@ -14,4 +49,4 @@ const binarySearch = (array, target) => {
 
 */
 
-module.exports = binarySearch
+module.exports = binarySearch;
